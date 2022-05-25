c779128e568bb2959193297f43f63599e615057f
sanjanaraghupathi <sanjana133.r@gmail.com>
2022-05-25 15:46:03 +0530

    problem10

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,13 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+f = "dataset/mbox-short.txt"
+c = open(f,"r")
+n = 0
+h = c.readlines()
+for i in h:
+    k = i.split()
+    if i.startswith("From:"):
+        print(k[1])
+        n+=1
+print(f"There were{n}lines in the file with From as the first word")
