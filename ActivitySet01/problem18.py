677e58dbe31881e01586aea8de2cd60a5e82af0a
sanjanaraghupathi <sanjana133.r@gmail.com>
2022-08-26 14:16:41 +0530

    done

--- ActivitySet01/problem18.py
+++ ActivitySet01/problem18.py
@@ -1,2 +1,31 @@
+#email
+import re
+pattern='(\w)+@(\w+\.)+(com)'
+email='jphn_123@gmail.com'
+s1=re.search(pattern,email)
+if s1:
+    print('search is successful')
+else:
+    print('unsuccessful')
+
+#phone number
+import re
+pattern='(0|91)?[6-9][0-9]{9}'
+p1='9901441075'
+s1=re.search(pattern,p1)
+if s1:
+    print('search is successful')
+else:
+    print('unsuccessful')
+
+
+import re
+pattern='^a...z$'
+word=input('enter a word')
+s1=re.match(pattern,word)
+if s1:
+    print('search is successful')
+else:
+    print('unsuccessful')
 
 
